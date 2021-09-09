44f21622790dc02816918963663f2924a13d5129
DianaShvetsova <dianashv@yandex.ru>
2021-09-09 18:52:04 +0300

    Homework 1.6

---
+++ src/pro/sky/java/course1/homework6/Main.java
@@ -0,0 +1,37 @@
+package pro.sky.java.course1.homework6;
+
+public class Main {
+    private static Object Array;
+
+    public static void main(String[] args) {
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("ФИО сотрудника – " + fullName);
+
+        task2();
+        task3();
+        task4();
+
+    }
+
+    private static void task2() {
+        String fullName = "Ivanov Ivan Ivanovich";
+        String forReport = fullName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета – " + forReport);
+    }
+
+    private static void task3() {
+        String fullName = "Ivanov Ivan Ivanovich";
+        String forPass = fullName.replace(" ", ";");
+        System.out.println("Данные ФИО сотрудника для административного отдела – " + forPass);
+    }
+
+    private static void task4() {
+        String fullName = "Иванов Семён Семёнович";
+        String rightName = fullName.replace("ё", "е");
+        System.out.println("Данные ФИО сотрудника – " + rightName);
+    }
+
+}
